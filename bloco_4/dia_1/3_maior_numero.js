04dccc759310e48cf4fdbc0409d098bd1236e3d1
Nicole Teisen Camargo <nicole.camargo@gmail.com>
2020-08-27 20:31:02 -0300

    exercicios de nota par e impar

--- bloco_4/dia_1/3_maior_numero.js
+++ bloco_4/dia_1/3_maior_numero.js
@@ -2,10 +2,10 @@ const a = 5;
 const b = 7;
 const c  =2;
 
-if (a > b && c) {
+if (a > b && a > c) {
     console.log(a);
 }
-else if (b > a && c) {
+else if (b > a && b > c) {
     console.log(b);
 }
 else {
